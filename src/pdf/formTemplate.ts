6d94cbbd275efdb235f832c2cf25016888717dde
Amin <amin@digitalpatterns.io>
2019-08-27 08:52:09 +0100

    added checksum

--- src/pdf/formTemplate.ts
+++ src/pdf/formTemplate.ts
@@ -1,13 +1,13 @@
 const template = `<html>
 <head>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
- <link rel="stylesheet" href="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.min.css" 
+ <link rel="stylesheet" href="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.min.css"
         integrity="sha384-qCmHtp5QGqc3DEUNAdcjETwaX3+JYREeRcBOEKW98JTIDWe3VZD9xBlaYc1/gmWZ" crossorigin="anonymous">
-    <script src="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.js" 
+    <script src="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.js"
         integrity="sha384-7Ohrz9GNTMS/34ninSvE7iUeX7D3hU8/NMXRc2APYW9JbpNneHepOXUUR2rNpsGC" crossorigin="anonymous">
-   
-</script> 
+
+</script>
     <script type='text/javascript'>
       window.onload = function() {
        Formio.createForm(document.getElementById('formio'),  <%- JSON.stringify(formSchema) %>).then(function(form) {
