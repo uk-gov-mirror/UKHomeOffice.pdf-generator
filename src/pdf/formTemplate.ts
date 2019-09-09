b90c7fe14ecb85cba82c166090e7ae794a074b7e
Amin <amin@digitalpatterns.io>
2019-09-09 12:38:26 +0100

    potential fix for page breaks

--- src/pdf/formTemplate.ts
+++ src/pdf/formTemplate.ts
@@ -1,5 +1,12 @@
 const template = `<html>
 <head>
+<style type="text/css" media="print">
+      div.page
+      {
+        page-break-after: always;
+        page-break-inside: avoid;
+      }
+</style>
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
  <link rel="stylesheet" href="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.min.css"
