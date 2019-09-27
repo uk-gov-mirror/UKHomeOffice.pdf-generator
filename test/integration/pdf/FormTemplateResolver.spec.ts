d3c10451e672a4972ae8181ced0d68fac1b04e96
Amin <amin@digitalpatterns.io>
2019-09-27 11:53:11 +0100

    used imports

--- test/integration/pdf/FormTemplateResolver.spec.ts
+++ test/integration/pdf/FormTemplateResolver.spec.ts
@@ -2,15 +2,11 @@ import {expect} from "chai";
 import {FormTemplateResolver} from "../../../src/pdf/FormTemplateResolver";
 import {basicForm} from "../../form";
 import {Substitute, SubstituteOf} from "@fluffy-spoon/substitute";
-import {S3Service} from "../../../src/service/S3Service";
-import {FormPdfGenerator} from "../../../src/pdf/FormPdfGenerator";
 import {KeycloakService} from "../../../src/service/KeycloakService";
 
 describe('FormTemplateResolver', () => {
 
     let formTemplateResolver: FormTemplateResolver;
-    let s3Service: SubstituteOf<S3Service>;
-    let formPdfGenerator: FormPdfGenerator;
     let keycloakService: SubstituteOf<KeycloakService>;
 
     beforeEach(() => {
