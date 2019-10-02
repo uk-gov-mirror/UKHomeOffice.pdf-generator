291e7a8fbf92c356d2e43b3295a467d772d8a785
Amin <amin@digitalpatterns.io>
2019-10-02 11:47:06 +0100

    upgraded formio

--- src/pdf/formTemplate.ts
+++ src/pdf/formTemplate.ts
@@ -11,8 +11,9 @@ const template = `<!doctype html>
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
  <link rel="stylesheet" href="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.min.css"
         integrity="sha384-qCmHtp5QGqc3DEUNAdcjETwaX3+JYREeRcBOEKW98JTIDWe3VZD9xBlaYc1/gmWZ" crossorigin="anonymous">
-    <script src="https://unpkg.com/formiojs@4.0.0-rc.27/dist/formio.full.js"
-        integrity="sha384-7Ohrz9GNTMS/34ninSvE7iUeX7D3hU8/NMXRc2APYW9JbpNneHepOXUUR2rNpsGC" crossorigin="anonymous">
+   <script src="https://unpkg.com/formiojs@4.2.8/dist/formio.full.js" 
+    integrity="sha384-oO8VpIg5Lz/nw2AjEeeu/KfV5PyGn3lhKfjBLMz/E358Sq7FI9Hmmec9tyyUowEs" 
+   crossorigin="anonymous"></script>
 
 </script>
     <script type='text/javascript'>
